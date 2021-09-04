094e98cde82f0659ddfe7a03549b475465191a91
Lonelypheonix <55979659+Lonelypheonix@users.noreply.github.com>
2021-09-04 14:42:32 +0530

    Add files via upload

---
+++ Program 10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//Pavan Kumar J 20201CCS0094
+
+int main()
+{
+  int l, w, area; 
+  printf("enter the length and width \n");
+  scanf("%d%d",&l,&w);
+  area=l*w;
+  printf("Area of rectangle = %d",area);
+  return 0;
+}
---
+++ Program 11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Pavan Kumar J 20201CCS0094
+
+int main()
+{
+  float rad,circum, area, pi=3.14159; 
+  printf("Enter the radius\n");
+ scanf("%f",&rad); 
+ circum=2*pi*rad;
+ area=pi*rad*rad;
+ printf("The circumference of circle is = %f",circum);
+ printf("\nThe area of circle is = %f",area);
+  return 0;
+}
