3228bb260d9ceb12813ae08378f9f7461f53e141
Lonelypheonix <55979659+Lonelypheonix@users.noreply.github.com>
2021-09-04 14:38:32 +0530

    Rename Output 3.c to Program 6.c

---
+++ Program 6.c
@@ -0,0 +1,15 @@
+//Pavan Kumar J 20201CCS0094 
+
+#include<stdio.h>
+void main()
+{
+ int x=15;
+ float y=3.5;
+ printf("x=%d \n",x);
+ printf("y=%f\n",y);
+ printf("The value of x is %d \n",x);
+ printf("The value of y is %f \n",y);
+ printf("x=%d and y=%f \n",x, y); 
+ printf("The value of x=%d and the value of y=%f",x, y);
+ } 
+ 
