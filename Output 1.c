6cd02f8fce75c42f96c3fb3dee878084be974c02
Lonelypheonix <55979659+Lonelypheonix@users.noreply.github.com>
2021-09-04 13:25:49 +0530

    Add files via upload

---
+++ Output 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Pavan Kumar J 20201CCS0094
+
+int main()
+{
+  printf("Name : Pavan Kumar J \n");
+  printf("Regno : 20201CCS0094 \n");
+  printf("Mobile no : 8050404347 \n");
+  printf("Department : Cyber security \n");
+  return 0;
+}
