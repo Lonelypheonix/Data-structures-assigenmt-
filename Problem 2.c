06144d9bb076cc31f14e5f60518db647c09f1e2a
Lonelypheonix <55979659+Lonelypheonix@users.noreply.github.com>
2021-09-04 14:20:50 +0530

    Add files via upload

---
+++ Problem 2.c
@@ -0,0 +1,10 @@
+//Pavan Kumar J 20201CCS0094
+
+#include<stdio.h>
+void main( )
+{
+printf("Storage size of int is %ld bytes \n" , sizeof(int));
+printf("Storage size of float is %ld bytes \n" , sizeof(float));
+printf("Storage size of char is %ld bytes \n" , sizeof(char));
+printf("Storage size of double is %ld bytes \n" , sizeof(double));
+}
