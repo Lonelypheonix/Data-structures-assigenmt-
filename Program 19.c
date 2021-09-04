83a026fc4f21471096a03d6caad24379b878c005
Lonelypheonix <55979659+Lonelypheonix@users.noreply.github.com>
2021-09-04 14:50:42 +0530

    Add files via upload

---
+++ Program 19.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//Pavan Kumar J 20201CCS0094
+void  main( )
+{
+int a,b,c,d,e,res;
+a=6; b=2; c=3; d=12; e=4;
+res=a*a+b-d/e+c;
+printf("res=%d",res);
+}
---
+++ Program 20.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//Pavan Kumar J 20201CCS0094
+void  main()
+{ 
+int p,q,r,w,x,res;
+p=3; r=6; q=2; w=14; x=3;
+res=p*r%q+w/x;
+printf("res=%d",res);
+}
