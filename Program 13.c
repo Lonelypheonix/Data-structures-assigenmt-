dca414c212479346f83487b18be4aa6af6544a89
Lonelypheonix <55979659+Lonelypheonix@users.noreply.github.com>
2021-09-04 14:44:14 +0530

    Add files via upload

---
+++ Program 13.c
@@ -0,0 +1,12 @@
+// Pavan Kumar J 20201CCS0094
+
+#include<stdio.h>
+void main()
+{
+ int a,b;
+ printf("enter the value of a\n");
+ scanf("%d",&a);
+ b=--a;
+ printf("after decrement a value is %d\n",a);
+ printf("b value is %d\n",b);
+}
---
+++ Program 14.c
@@ -0,0 +1,16 @@
+// Pavan Kumar J 20201CCS0094
+
+#include<stdio.h>
+void main()
+{
+ int a,b,res1,res2;
+ printf("Enter the values of a and b\n");
+ scanf("%d%d",&a,&b);
+ printf("Original value of a is %d, b is %d\n",a,b);
+ res1=++a + --b;
+ printf("After pre increment of a and pre decrement of b\n");
+ printf(" a=%d, b=%d, res1=%d\n",a,b,res1);
+ res2=a++ + b--;
+ printf("After post increment of a and post decrement of b\n");
+ printf(" a=%d, b=%d, res2=%d\n",a,b,res2);
+}
